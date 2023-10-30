12d9a765297a6c6bf6e8534622a54ff5903d5869
STP <adam.a.french@outlook.com>
2023-10-30 17:55:01 -0400

    test

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,23 @@
 <template>
-  <Three />
+  <h1 class="title">Hello</h1>
+  <div id="canvas">
+    <Three />
+  </div>
 </template>
 
 <style>
 body {
+  background-color: cadetblue;
   background-size: 5%;
   background-repeat: repeat;
+}
+
+.title {
+  font-family: "Courier New", Courier, monospace;
+}
+
+#canvas {
+  width: 60vh;
+  height: 50vh;
 }
 </style>
